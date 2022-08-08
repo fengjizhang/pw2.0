83895132fdaa95c5bb3fd26efb992ee38ecabc9e
Fengji Zhang <fengjizhang@Fengjis-MacBook-Air.local>
2022-08-08 19:49:29 -0400

    added contact and footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import '../css files/contact.css';
-import { AiFillMail, AiFillCompass } from "react-icons/ai";
+import { AiFillMail, AiFillCompass,AiFillGithub,AiFillLinkedin } from "react-icons/ai";
+import { IconContext } from "react-icons";
+
 const Contact = (props) => {
 
     return (
@@ -9,21 +11,44 @@ const Contact = (props) => {
             <div className='contact3'>So you can get to know me a bit more</div>
 
             <div className='infoContainer'>
+
+                <div className='blockContainer'>
+                    <IconContext.Provider value={{ color: "white", size: '50px', className: "contactIcons" }}>
+                        <AiFillCompass />
+                    </IconContext.Provider>
+                    <div className='contact5'>Location:
+                        <div className='contact6'>Columbus, OH</div>
+                    </div>
+                </div>
                 
-                <div className='contact5'>Location
-                    <div className='contact6'>Columbus, OH</div>
+                <div className='blockContainer'>
+                    <IconContext.Provider value={{ color: "white", size: '50px', className: "contactIcons" }}>
+                        <AiFillMail />
+                    </IconContext.Provider>
+                    
+                    <div className='contact5'>Email:
+                        <div className='contact6'>fengji.zhang0@gmail.com</div>
+                    </div>
                 </div>
-
-                <div className='contact5'>Email
-                    <div className='contact6'>fengji.zhang0@gmail.com</div>
+                
+                <div className='blockContainer'>
+                    <IconContext.Provider value={{ color: "white", size: '50px', className: "contactIcons" }}>
+                        <AiFillLinkedin />
+                    </IconContext.Provider>
+                    
+                    <div className='contact5'>Let's Connect!
+                        <div className='contact6'><a className="linktext" href="https://www.linkedin.com/in/fengjizhang/">LinkedIn</a></div>
+                    </div>
                 </div>
-
-                <div className='contact5'>Let's Connect
-                    <div className='contact6'>link</div>
-                </div>
-
-                <div className='contact5'>Github
-                    <div className='contact6'>link</div>
+                
+                <div className='blockContainer'>
+                    <IconContext.Provider value={{ color: "white", size: '50px', className: "contactIcons" }}>
+                        <AiFillGithub />
+                    </IconContext.Provider>
+                    
+                    <div className='contact5'>Portfolio link
+                        <div className='contact6'><a className="linktext" href="https://github.com/fengjizhang">Github</a></div>
+                    </div>
                 </div>
                 
             </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
+import '../css files/footer.css';
 
 
 const Footer = (props) => {
 
     return (
         <div className="footer">
-            <h4>Footer</h4>
+            <h4>Created by Fengji Zhang</h4>
+
             
         </div>
     )
